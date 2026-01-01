4d6d086465a021d7970d1ce4a694faf35ef9354c
Baskar022 <baskarpillai768@gmail.com>
2026-01-01 20:27:01 +0530

    updated operators

--- java_basis/operators.java
+++ java_basis/operators.java
@@ -1,5 +1,6 @@
 public class operators {
     public static void main(String[] args) {
+        /*
         int a = 11, b = 5;
         int sum = a + b; //same for -,*,/
         int remainder = a%b;
@@ -10,6 +11,23 @@ public class operators {
         ++result; //-5
         System.out.println(result); //-5
 
-        System.out.println(~num1);
+        System.out.println(~num1);//bitewise complement
+        */
+       int n1 = 10;
+       double n2 = 5.7;
+       n1 += n2; // no error, 15
+    //    n2 += n1; // no error, 20.7
+        n2 = n2 + 10; // no er, 15.7
+        // n1 = n1 + 5.5; //RT er
+        n1 = (int) (n1 + 5.5); // no err, 20
+       System.out.println(n1 + " and " + n2);
+       String s1 = "10";
+    //    boolean result = n1!=s1; // err
+    boolean result = n1!=n2; 
+       System.out.println(result);
+    //    System.out.println(n1 && n2); //err, type is int, double not boolean or condition
+    double n3 = 7.9;
+        System.out.println(n3 instanceof double);
+        System.out.println(n2 instanceof int);
     }
 }
