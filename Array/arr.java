9c8c2363175bb9825d13ad0123dc12db2aa62d30
Baskar022 <baskarpillai768@gmail.com>
2026-01-15 21:07:26 +0530

    array

--- Array/arr.java
+++ Array/arr.java
@@ -1,5 +1,11 @@
 package Array;
 
 public class arr {
-    
+    public static void main(String[] args) {
+        // String name[10]; //err
+        String name[] = new String[10];
+        int roll[];
+        roll = new int[10];
+        
+    }
 }
